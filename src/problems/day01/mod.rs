1fc578062fed5a77e9827108d8644451a618939a
Graham Keenan <graham.keenan01@bbc.co.uk>
2023-12-01 12:10:13 +0000

    feat: day01 - part1 complete

--- src/problems/day01/mod.rs
+++ src/problems/day01/mod.rs
@@ -1,11 +1,14 @@
 use crate::utils::*;
 
 // Add your own custom fields for the solution here
-pub struct Solution {}
+pub struct Solution {
+    codes: Vec<String>,
+}
 
 impl Solution {
     pub fn new() -> Self {
-        let mut sol = Self {};
+        let mut sol = Self { codes: vec![] };
+
         sol.process_input("day01/input.txt");
         sol
     }
@@ -14,14 +17,33 @@ impl Solution {
 impl Solve for Solution {
     // Perform any manipulations to the input here
     fn process_input(&mut self, path: &str) {
-        let _raw = read_file(path);
+        let raw = read_file(path);
+        self.codes = raw
+            .split('\n')
+            .filter(|l| !l.is_empty())
+            .map(|l| l.to_string())
+            .collect::<Vec<String>>();
     }
 
     fn part1(&mut self) {
-        println!("Day01 - Part 1: Edit me to start!");
+        let total: i32 = self
+            .codes
+            .iter()
+            .map(|code| {
+                let numbers = code
+                    .chars()
+                    .into_iter()
+                    .filter(|c| c.is_numeric())
+                    .collect::<Vec<char>>();
+
+                format!("{}{}", numbers[0], numbers[numbers.len() - 1])
+                    .parse::<i32>()
+                    .unwrap()
+            })
+            .sum();
+
+        println!("Day 01 / Part 1: {total}");
     }
 
-    fn part2(&mut self) {
-        println!("Day01 - Part 2: Edit me to start!");
-    }
+    fn part2(&mut self) {}
 }
