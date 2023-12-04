6447b44eb969ebc87f4e639a7d1a3b1345421a67
Graham Keenan <graham.keenan@outlook.com>
2023-12-04 17:19:16 +0000

    feat: day04 - part 1 complete

--- src/problems/day04/mod.rs
+++ src/problems/day04/mod.rs
@@ -1,27 +1,75 @@
 use crate::utils::*;
+use std::collections::HashSet;
 
-// Add your own custom fields for the solution here
-pub struct Solution {}
+#[derive(Debug)]
+pub struct Card {
+    winner: HashSet<i32>,
+    numbers: HashSet<i32>,
+}
+
+impl Card {
+    pub fn new(winner: &str, numbers: &str) -> Self {
+        let winner = winner
+            .split_whitespace()
+            .map(|n| n.trim().parse::<i32>().unwrap())
+            .collect::<HashSet<i32>>();
+
+        let numbers = numbers
+            .split_whitespace()
+            .map(|n| n.trim().parse::<i32>().unwrap())
+            .collect::<HashSet<i32>>();
+
+        return Self { winner, numbers };
+    }
+}
+
+pub struct Solution {
+    cards: Vec<Card>,
+}
 
 impl Solution {
     pub fn new() -> Self {
-        let mut sol = Self {};
+        let mut sol = Self { cards: vec![] };
         sol.process_input("day04/input.txt");
         sol
     }
 }
 
+fn power_it(times: usize) -> usize {
+    let mut total = 1;
+    for _ in 0..times - 1 {
+        total *= 2;
+    }
+
+    return total;
+}
+
 impl Solve for Solution {
-    // Perform any manipulations to the input here
     fn process_input(&mut self, path: &str) {
-        let _raw = read_file(path);
+        for line in read_file(path).lines() {
+            let (_, card_numbers) = line.split_once(": ").unwrap();
+            let (winner, numbers) = card_numbers.split_once(" | ").unwrap();
+            let card = Card::new(winner, numbers);
+            self.cards.push(card);
+        }
     }
 
     fn part1(&mut self) {
-        println!("Day04 - Part 1: Edit me to start!");
+        let total = self
+            .cards
+            .iter()
+            .map(|c| {
+                let winners = c.winner.intersection(&c.numbers).count();
+                if winners == 0 {
+                    0
+                } else {
+                    power_it(winners)
+                }
+            })
+            .sum::<usize>();
+
+        println!("Day 04 / Part 1 - {total}");
     }
 
-    fn part2(&mut self) {
-        println!("Day04 - Part 2: Edit me to start!");
-    }
+    fn part2(&mut self) {}
 }
