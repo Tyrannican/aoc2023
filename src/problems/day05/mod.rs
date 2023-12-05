f94abe135411f584ca8ee922776c562738e5ecd3
Graham Keenan <graham.keenan@outlook.com>
2023-12-05 20:04:55 +0000

    feat: day 05 - part 1 complete

--- src/problems/day05/mod.rs
+++ src/problems/day05/mod.rs
@@ -1,27 +1,99 @@
 use crate::utils::*;
 
-// Add your own custom fields for the solution here
-pub struct Solution {}
+type MapRange = (i64, i64, i64);
+
+pub struct Solution {
+    seeds: Vec<i64>,
+    maps: Vec<Vec<MapRange>>,
+}
 
 impl Solution {
     pub fn new() -> Self {
-        let mut sol = Self {};
+        let mut sol = Self {
+            seeds: vec![],
+            maps: vec![],
+        };
         sol.process_input("day05/input.txt");
         sol
     }
+
+    pub fn get_final_location(&self, mut seed: i64) -> i64 {
+        for map in self.maps.iter() {
+            for ranges in map.into_iter() {
+                let (dst, src, rng) = ranges;
+                if seed >= *src && seed <= *src + rng {
+                    let offset = i64::abs(src - dst);
+                    let value = if src < dst {
+                        seed + offset
+                    } else {
+                        seed - offset
+                    };
+                    seed = value;
+                    break;
+                }
+            }
+        }
+
+        return seed;
+    }
+}
+
+fn get_seeds(s: &str) -> Vec<i64> {
+    let (_, seeds) = s.split_once(": ").unwrap();
+    return seeds
+        .split_whitespace()
+        .into_iter()
+        .map(|s| s.parse::<i64>().unwrap())
+        .collect();
+}
+
+fn build_map_range(s: &str) -> MapRange {
+    let ranges = s
+        .split_whitespace()
+        .into_iter()
+        .map(|s| s.parse::<i64>().unwrap())
+        .collect::<Vec<i64>>();
+
+    return (ranges[0], ranges[1], ranges[2]);
 }
 
 impl Solve for Solution {
-    // Perform any manipulations to the input here
     fn process_input(&mut self, path: &str) {
-        let _raw = read_file(path);
+        let raw = read_file(path);
+        let mut maps = raw.split("\n\n").collect::<Vec<&str>>();
+        self.seeds = get_seeds(&maps.remove(0));
+
+        self.maps = maps
+            .iter()
+            .map(|m| {
+                let mut split = m
+                    .split("\n")
+                    .filter_map(|s| {
+                        if s.is_empty() {
+                            return None;
+                        }
+
+                        return Some(s);
+                    })
+                    .collect::<Vec<&str>>();
+
+                // Ditch the name
+                split.remove(0);
+                split.into_iter().map(|n| build_map_range(n)).collect()
+            })
+            .collect::<Vec<Vec<MapRange>>>();
     }
 
     fn part1(&mut self) {
-        println!("Day05 - Part 1: Edit me to start!");
+        let lowest = self
+            .seeds
+            .iter()
+            .map(|s| self.get_final_location(*s))
+            .min()
+            .unwrap_or(-1);
+
+        println!("Day 05 / Part 1: {lowest}");
     }
 
-    fn part2(&mut self) {
-        println!("Day05 - Part 2: Edit me to start!");
-    }
+    fn part2(&mut self) {}
 }
