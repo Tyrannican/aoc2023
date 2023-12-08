b6c5fa0f750b135fcb3d5881129b1c61a20c199b
Graham Keenan <graham.keenan@outlook.com>
2023-12-08 16:12:59 +0000

    feat: day 07 - part 2 complete (got there...)

--- src/problems/day07/mod.rs
+++ src/problems/day07/mod.rs
@@ -109,6 +109,24 @@ impl CardValue {
     pub fn cards_to_values(cards: &str) -> Vec<Self> {
         return cards.chars().map(|c| CardValue::from_char(c)).collect();
     }
+
+    pub fn custom_cmp(&self, other: &Self) -> Ordering {
+        use CardValue::*;
+        match (&self, &other) {
+            (A, A) | (K, K) | (Q, Q) | (J, J) | (T, T) => Ordering::Equal,
+            (A, _) => Ordering::Greater,
+            (_, A) => Ordering::Less,
+            (K, _) => Ordering::Greater,
+            (_, K) => Ordering::Less,
+            (Q, _) => Ordering::Greater,
+            (_, Q) => Ordering::Less,
+            (T, _) => Ordering::Greater,
+            (_, T) => Ordering::Less,
+            (Num(i), Num(j)) => i.cmp(j),
+            (J, _) => Ordering::Less,
+            (_, J) => Ordering::Greater,
+        }
+    }
 }
 
 impl Eq for CardValue {}
@@ -124,16 +142,16 @@ impl Ord for CardValue {
 
         match (&self, &other) {
             (A, A) | (K, K) | (Q, Q) | (J, J) | (T, T) => Ordering::Equal,
-            (A, _) => std::cmp::Ordering::Greater,
-            (_, A) => std::cmp::Ordering::Less,
-            (K, _) => std::cmp::Ordering::Greater,
-            (_, K) => std::cmp::Ordering::Less,
-            (Q, _) => std::cmp::Ordering::Greater,
-            (_, Q) => std::cmp::Ordering::Less,
-            (J, _) => std::cmp::Ordering::Greater,
-            (_, J) => std::cmp::Ordering::Less,
-            (T, _) => std::cmp::Ordering::Greater,
-            (_, T) => std::cmp::Ordering::Less,
+            (A, _) => Ordering::Greater,
+            (_, A) => Ordering::Less,
+            (K, _) => Ordering::Greater,
+            (_, K) => Ordering::Less,
+            (Q, _) => Ordering::Greater,
+            (_, Q) => Ordering::Less,
+            (J, _) => Ordering::Greater,
+            (_, J) => Ordering::Less,
+            (T, _) => Ordering::Greater,
+            (_, T) => Ordering::Less,
             (Num(i), Num(j)) => i.cmp(j),
         }
     }
@@ -147,41 +165,83 @@ struct Hand {
 }
 
 impl Hand {
+    // Stepwise check
     pub fn is_better_than(&self, other: &Self) -> Ordering {
         for (v, o) in self.values.iter().zip(other.values.iter()) {
             if v == o {
                 continue;
-            } else if v > o {
-                return Ordering::Greater;
             }
 
-            return Ordering::Less;
+            return v.cmp(o);
+        }
+
+        return Ordering::Equal;
+    }
+
+    // Custom rule for Jokers
+    fn is_better_than_revised(&self, other: &Self) -> Ordering {
+        for (v, o) in self.values.iter().zip(other.values.iter()) {
+            if v == o {
+                continue;
+            }
+
+            return v.custom_cmp(o);
         }
 
         return Ordering::Equal;
     }
 
     // Recalculate based on the Joker rule in Part 2
-    // Todo:: fix
     fn reevaluate_hand(&mut self) {
-        let mut values = self.values.clone();
-        let mut counter: HashMap<CardValue, usize> = HashMap::new();
-        for value in values.clone().into_iter() {
-            counter.entry(value).and_modify(|v| *v += 1).or_insert(1);
+        if !self.values.contains(&CardValue::J) {
+            return;
         }
 
-        // Determine how to deal with issues when there is only one of a given type
-        // e.g. [2, Q, A, J, 5] -> [2, Q, A, J, 5]
-        if counter.contains_key(&CardValue::J) {
-            let (mut largest_key, total) = counter.iter().max_by_key(|k| k.1).unwrap();
-            for (pos, value) in self.values.iter().enumerate() {
-                if *value == CardValue::J {
-                    values[pos] = largest_key.clone();
+        let mut ranked_hand = self.values.clone();
+        ranked_hand.sort_by(|a, b| b.custom_cmp(&a));
+        let mut highest_rank = &ranked_hand[0];
+
+        let mut counts = HashMap::new();
+
+        for value in self.values.iter() {
+            counts.entry(value).and_modify(|v| *v += 1).or_insert(1);
+        }
+
+        match self.hand_type {
+            HandType::Pair => {
+                for (k, v) in counts.iter() {
+                    if *v == 2 && *k != &CardValue::J {
+                        highest_rank = k;
+                    }
                 }
+            }
+            HandType::TwoPair => {
+                for (k, v) in counts.iter() {
+                    if *v == 2 && *k != &CardValue::J {
+                        highest_rank = k;
+                    }
+                }
+            }
+            HandType::ThreeKind => {
+                for (k, v) in counts.iter() {
+                    if *v == 3 && *k != &CardValue::J {
+                        highest_rank = k;
+                    }
+                }
+            }
+            _ => {}
+        }
+
+        let mut check_values = self.values.clone();
+        for (pos, value) in self.values.iter().enumerate() {
+            if value == &CardValue::J {
+                check_values[pos] = highest_rank.clone();
             }
         }
 
-        self.hand_type = HandType::from_cards(&values);
+        let new_hand = HandType::from_cards(&check_values);
+
+        self.hand_type = new_hand;
     }
 }
 
@@ -194,6 +254,10 @@ impl Solution {
         let mut sol = Self { hands: vec![] };
         sol.process_input("day07/input.txt");
         sol
+    }
+
+    pub fn reset(&mut self) {
+        self.process_input("day07/input.txt");
     }
 }
 
@@ -237,13 +301,14 @@ impl Solve for Solution {
     }
 
     fn part2(&mut self) {
+        self.reset();
         let mut result = 0;
         let mut current_rank = 1;
 
         self.hands.iter_mut().for_each(|h| h.reevaluate_hand());
         self.hands.sort_by(|a, b| {
             if a.hand_type == b.hand_type {
-                return a.is_better_than(b);
+                return a.is_better_than_revised(b);
             }
 
             a.hand_type.cmp(&b.hand_type)
@@ -253,5 +318,7 @@ impl Solve for Solution {
             result += current_rank * hand.bid;
             current_rank += 1;
         }
+
+        println!("Day 07 / Part 2: {result}");
     }
 }
