7a7856923cef2ea83cac310520cc26dc6e936dec
Graham Keenan <graham.keenan@outlook.com>
2023-12-12 19:51:25 +0000

    feat: day10 - part 1 complete

--- src/problems/day10/mod.rs
+++ src/problems/day10/mod.rs
@@ -1,27 +1,114 @@
 use crate::utils::*;
+use kurve::Kurve;
+use std::collections::HashSet;
 
-// Add your own custom fields for the solution here
-pub struct Solution {}
+type Coord = (usize, usize);
+
+pub struct Solution {
+    start: Coord,
+    map: Kurve<Coord, char>,
+}
 
 impl Solution {
     pub fn new() -> Self {
-        let mut sol = Self {};
+        let mut sol = Self {
+            start: (0, 0),
+            map: Kurve::new(),
+        };
         sol.process_input("day10/input.txt");
         sol
     }
 }
 
 impl Solve for Solution {
-    // Perform any manipulations to the input here
     fn process_input(&mut self, path: &str) {
-        let _raw = read_file(path);
+        for (i, line) in read_file(path).lines().enumerate() {
+            for (j, char) in line.chars().enumerate() {
+                let src = (i, j);
+                if char == '.' {
+                    continue;
+                }
+                self.map.add_vertex(src, char);
+
+                match char {
+                    '|' => {
+                        self.map.add_edge(src, (i + 1, j));
+                        self.map.add_edge(src, (i - 1, j));
+                    }
+                    '-' => {
+                        self.map.add_edge(src, (i, j - 1));
+                        self.map.add_edge(src, (i, j + 1));
+                    }
+                    'L' => {
+                        self.map.add_edge(src, (i - 1, j));
+                        self.map.add_edge(src, (i, j + 1));
+                    }
+                    'J' => {
+                        self.map.add_edge(src, (i, j - 1));
+                        self.map.add_edge(src, (i - 1, j));
+                    }
+                    '7' => {
+                        self.map.add_edge(src, (i, j - 1));
+                        self.map.add_edge(src, (i + 1, j));
+                    }
+                    'F' => {
+                        self.map.add_edge(src, (i, j + 1));
+                        self.map.add_edge(src, (i + 1, j));
+                    }
+                    'S' => self.start = src,
+                    _ => {}
+                }
+            }
+        }
+
+        // Set neighbors for the start position (S)
+        let start = self.start.clone();
+        let vertices = self.map.get_all_neighbors();
+        for (vtx, n) in vertices.into_iter() {
+            if n.contains_key(&start) {
+                self.map.add_edge(start, vtx);
+            }
+        }
     }
 
     fn part1(&mut self) {
-        println!("Day10 - Part 1: Edit me to start!");
+        let mut current = self.start.clone();
+        let mut seen: HashSet<Coord> = HashSet::new();
+        let mut steps = 0;
+        loop {
+            seen.insert(current);
+            let neighbors: Vec<Coord> = self
+                .map
+                .get_neighbors(current)
+                .unwrap()
+                .into_keys()
+                .collect();
+
+            let mut iter = neighbors.iter();
+            let mut next = iter.next().unwrap();
+
+            let mut finished = false;
+            while seen.contains(next) {
+                match iter.next() {
+                    Some(n) => next = n,
+                    None => {
+                        finished = true;
+                        steps += 1;
+                        break;
+                    }
+                }
+            }
+
+            if finished && iter.next().is_none() {
+                break;
+            }
+
+            current = *next;
+            steps += 1;
+        }
+
+        println!("Steps: {}", steps / 2);
     }
 
-    fn part2(&mut self) {
-        println!("Day10 - Part 2: Edit me to start!");
-    }
+    fn part2(&mut self) {}
 }
