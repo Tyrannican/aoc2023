2cd553f7d1b4e97fa000dc80807f48fdf7f5abe5
Graham Keenan <graham.keenan@outlook.com>
2023-12-15 17:38:18 +0000

    feat: day 12 part 2 complete - thank god

--- src/problems/day12/mod.rs
+++ src/problems/day12/mod.rs
@@ -1,13 +1,14 @@
 use crate::utils::*;
+use std::collections::HashMap;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 pub enum Status {
     Operational,
     Damaged,
     Unknown,
 }
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 pub struct Record {
     config: Vec<usize>,
     springs: Vec<Status>,
@@ -15,7 +16,16 @@ pub struct Record {
 
 impl Record {
     // Taken from https://github.com/andypymont/advent2023-rust/blob/main/src/bin/12.rs
-    pub fn arrangements(&self, spring_idx: usize, cfg_idx: usize) -> u64 {
+    pub fn arrangements(
+        &self,
+        spring_idx: usize,
+        cfg_idx: usize,
+        cache: &mut HashMap<(usize, usize), u64>,
+    ) -> u64 {
+        if let Some(val) = cache.get(&(spring_idx, cfg_idx)) {
+            return *val;
+        }
+
         let consume_group = self.config.get(cfg_idx).map_or(0, |c_len| {
             if (spring_idx + c_len) > self.springs.len() {
                 return 0;
@@ -31,16 +41,41 @@ impl Record {
                 return 0;
             }
 
-            self.arrangements(spring_idx + c_len + 1, cfg_idx + 1)
+            self.arrangements(spring_idx + c_len + 1, cfg_idx + 1, cache)
         });
 
         let skip = match self.springs.get(spring_idx) {
             None => u64::from(cfg_idx >= self.config.len()),
             Some(Status::Damaged) => 0,
-            Some(_) => self.arrangements(spring_idx + 1, cfg_idx),
+            Some(_) => self.arrangements(spring_idx + 1, cfg_idx, cache),
         };
 
-        return consume_group + skip;
+        let arrangements = consume_group + skip;
+        cache.insert((spring_idx, cfg_idx), arrangements);
+
+        return arrangements;
+    }
+
+    pub fn total_arrangements(&self) -> u64 {
+        let mut cache = HashMap::new();
+        return self.arrangements(0, 0, &mut cache);
+    }
+
+    pub fn expand(self) -> Self {
+        let expanded_config = self.config.repeat(5);
+        let mut expanded_springs = vec![];
+
+        for i in 0..5 {
+            expanded_springs.extend(self.springs.clone());
+            if i != 4 {
+                expanded_springs.push(Status::Unknown);
+            }
+        }
+
+        return Self {
+            springs: expanded_springs,
+            config: expanded_config,
+        };
     }
 }
 
@@ -84,11 +119,19 @@ impl Solve for Solution {
         let total = self
             .records
             .iter()
-            .map(|r| r.arrangements(0, 0))
+            .map(|r| r.total_arrangements())
             .sum::<u64>();
 
         println!("Day 12 / Part 1: {total}");
     }
 
-    fn part2(&mut self) {}
+    fn part2(&mut self) {
+        let records = self.records.clone();
+        let total = records
+            .into_iter()
+            .map(|r| r.expand().total_arrangements())
+            .sum::<u64>();
+
+        println!("Day 12 / Part 2: {total}");
+    }
 }
