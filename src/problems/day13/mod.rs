5195952516a49b7fa8aa540eec4abddadc3a265b
Graham Keenan <graham.keenan@outlook.com>
2023-12-15 18:06:19 +0000

    working on day 13 part 1

--- src/problems/day13/mod.rs
+++ src/problems/day13/mod.rs
@@ -1,10 +1,39 @@
 use crate::utils::*;
 
-pub struct Solution {}
+type MirrorIdx = (usize, usize);
+
+#[derive(Debug, PartialEq)]
+pub enum MirrorPlane {
+    Horizontal(MirrorIdx),
+    Vertical(MirrorIdx),
+    None,
+}
+
+pub struct AshField {
+    grid: Vec<Vec<char>>,
+}
+
+impl AshField {
+    pub fn mirror_plane(&self) -> MirrorPlane {
+        return MirrorPlane::None;
+    }
+
+    fn horizontal(&self) -> MirrorPlane {
+        return MirrorPlane::None;
+    }
+
+    fn vertical(&self) -> MirrorPlane {
+        return MirrorPlane::None;
+    }
+}
+
+pub struct Solution {
+    ash_fields: Vec<AshField>,
+}
 
 impl Solution {
     pub fn new() -> Self {
-        let mut sol = Self {};
+        let mut sol = Self { ash_fields: vec![] };
         sol.process_input("day13/input.txt");
         sol
     }
@@ -12,7 +41,15 @@ impl Solution {
 
 impl Solve for Solution {
     fn process_input(&mut self, path: &str) {
-        let _raw = read_file(path);
+        self.ash_fields = read_file(path)
+            .split("\n\n")
+            .map(|f| AshField {
+                grid: f
+                    .lines()
+                    .map(|l| l.chars().collect::<Vec<char>>())
+                    .collect::<Vec<Vec<char>>>(),
+            })
+            .collect::<Vec<AshField>>();
     }
 
     fn part1(&mut self) {}
