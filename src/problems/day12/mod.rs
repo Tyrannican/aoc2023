86b9a56a0c0ada48312c4fb74603103dea87aa0d
Graham Keenan <graham.keenan@outlook.com>
2023-12-15 17:13:30 +0000

    feat: day 12 part 1 complete - had to reverse a solution for it
    My brain just ain't wired for dynamic programming ;_;

--- src/problems/day12/mod.rs
+++ src/problems/day12/mod.rs
@@ -1,9 +1,47 @@
 use crate::utils::*;
 
+#[derive(Debug, PartialEq)]
+pub enum Status {
+    Operational,
+    Damaged,
+    Unknown,
+}
+
 #[derive(Debug)]
 pub struct Record {
     config: Vec<usize>,
-    springs: String,
+    springs: Vec<Status>,
+}
+
+impl Record {
+    // Taken from https://github.com/andypymont/advent2023-rust/blob/main/src/bin/12.rs
+    pub fn arrangements(&self, spring_idx: usize, cfg_idx: usize) -> u64 {
+        let consume_group = self.config.get(cfg_idx).map_or(0, |c_len| {
+            if (spring_idx + c_len) > self.springs.len() {
+                return 0;
+            }
+
+            if (0..*c_len)
+                .any(|idx| self.springs.get(spring_idx + idx) == Some(&Status::Operational))
+            {
+                return 0;
+            }
+
+            if self.springs.get(spring_idx + c_len) == Some(&Status::Damaged) {
+                return 0;
+            }
+
+            self.arrangements(spring_idx + c_len + 1, cfg_idx + 1)
+        });
+
+        let skip = match self.springs.get(spring_idx) {
+            None => u64::from(cfg_idx >= self.config.len()),
+            Some(Status::Damaged) => 0,
+            Some(_) => self.arrangements(spring_idx + 1, cfg_idx),
+        };
+
+        return consume_group + skip;
+    }
 }
 
 pub struct Solution {
@@ -23,32 +61,34 @@ impl Solve for Solution {
         for line in read_file(path).lines() {
             let (springs, config) = line.split_once(' ').unwrap();
 
+            let springs = springs
+                .chars()
+                .filter_map(|c| match c {
+                    '#' => Some(Status::Damaged),
+                    '.' => Some(Status::Operational),
+                    '?' => Some(Status::Unknown),
+                    _ => None,
+                })
+                .collect();
+
             let config = config
                 .split(',')
                 .map(|n| n.parse::<usize>().unwrap())
                 .collect::<Vec<usize>>();
 
-            self.records.push(Record {
-                springs: springs.to_string(),
-                config,
-            });
+            self.records.push(Record { springs, config });
         }
     }
 
-    fn part1(&mut self) {}
+    fn part1(&mut self) {
+        let total = self
+            .records
+            .iter()
+            .map(|r| r.arrangements(0, 0))
+            .sum::<u64>();
+
+        println!("Day 12 / Part 1: {total}");
+    }
 
     fn part2(&mut self) {}
-}
-
-fn arrangements(s: &str) -> u64 {
-    let unknowns = s
-        .chars()
-        .enumerate()
-        .filter_map(|(idx, c)| match c {
-            '?' => Some(idx),
-            _ => None,
-        })
-        .collect::<Vec<usize>>();
-
-    return 0;
 }
